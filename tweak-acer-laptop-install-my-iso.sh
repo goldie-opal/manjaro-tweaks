09fa8ceb247d0ae90eb7d42188bdda0250fbad1f
Goldie Opal <34083352+goldie-opal@users.noreply.github.com>
2021-05-08 19:46:56 +1000

    added acer latop tweak, updated iso package list

--- tweak-acer-laptop-install-my-iso.sh
+++ tweak-acer-laptop-install-my-iso.sh
@@ -12,9 +12,8 @@ function installSublime() {
     sudo pacman -Sy --noconfirm --needed sublime-text
 }
 
-function installNvidiaDriversOptimusManager() {
-	# Install nvidia-440xx drivers and Optimus Manager
-	sudo pacman -S --noconfirm --needed linux56-nvidia-440xx nvidia-440xx-utils linux56-bbswitch lib32-virtualgl lib32-nvidia-440xx-utils xf86-video-nouveau 
+function installOptimusManager() {
+	# Install Optimus Manager
 	sudo pacman -S --noconfirm --needed optimus-manager 
 	sudo systemctl enable optimus-manager.service
 	yay -S --noconfirm optimus-manager-qt
@@ -24,19 +23,6 @@ function setHardwareClock() {
 	# Configure hw clock to use localtime for dual booting
 	sudo timedatectl set-local-rtc 1 
 	sudo hwclock --systohc --local
-}
-
-function installBlueToothDriver() {
-	# Install headers and dkms
-	sudo pacman -S --noconfirm --needed linux56-headers dkms
-	# Install Bluetooth Driver 
-	yay -S --noconfirm rtbth-dkms-git
-	sudo touch /etc/modules-load.d/rtbth.conf
-	sudo su -c 'echo -e "rtbth" > /etc/modules-load.d/rtbth.conf'
-}
-
-function installVirtualBox() {
-	sudo pacman -S --needed --noconfirm virtualbox linux56-virtualbox-host-modules
 }
 
 function setPeriodicTrim() {
@@ -53,9 +39,7 @@ function setSilentBoot() {
 }
 
 #installSublime
-installNvidiaDriversOptimusManager
+installOptimusManager
 setHardwareClock
-installBlueToothDriver
-installVirtualBox
 setPeriodicTrim
 setSilentBoot
